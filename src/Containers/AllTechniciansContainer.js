9163a39fd8eb10cb0169b2003d46dbcb6b18bd44
Shane Walsh <shanewalsh@hey.com>
2021-09-29 16:03:59 +0100

    lint containter

--- src/Containers/AllTechniciansContainer.js
+++ src/Containers/AllTechniciansContainer.js
@@ -1,79 +1,79 @@
-import React, { Component } from "react"
-import API from "../Adaptors/API"
-import TechnicianCard from "../Components/TechnicianCard"
-import { TextField, Grid, Typography } from "@material-ui/core"
-import CircularProgress from "@material-ui/core/CircularProgress"
-import Search from "@material-ui/icons/Search"
+import React, { Component } from "react";
+import API from "../Adaptors/API";
+import TechnicianCard from "../Components/TechnicianCard";
+import { TextField, Grid, Typography } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Search from "@material-ui/icons/Search";
 
 class AllTechniciansContainer extends Component {
   state = {
     allTechnicians: [],
     searchField: "",
-    loading: true
-  }
+    loading: true,
+  };
 
   componentDidMount = () => {
     if (!this.props.match.params.id) {
-      API.getAllTechnicians().then(allTechnicians =>
+      API.getAllTechnicians().then((allTechnicians) =>
         this.setState({ allTechnicians, loading: false })
-      )
+      );
     } else {
       API.getAvailableTechnicians(this.props.match.params.id).then(
-        availableTechnicians =>
+        (availableTechnicians) =>
           this.setState({
             allTechnicians: availableTechnicians,
-            loading: false
+            loading: false,
           })
-      )
+      );
     }
-  }
+  };
 
   searchByName = () => {
-    const { searchField, allTechnicians } = this.state
-    if (searchField === "") return allTechnicians
-    return allTechnicians.filter(tech =>
+    const { searchField, allTechnicians } = this.state;
+    if (searchField === "") return allTechnicians;
+    return allTechnicians.filter((tech) =>
       tech.name.toLowerCase().includes(searchField.toLowerCase())
-    )
-  }
+    );
+  };
 
-  handleChange = event => this.setState({ searchField: event.target.value })
+  handleChange = (event) => this.setState({ searchField: event.target.value });
 
-  handleFilter = event => {
+  handleFilter = (event) => {
     if (!this.state.disciplineFilters.includes(event.target.value)) {
       this.setState({
         disciplineFilters: [
           ...this.state.disciplineFilters,
-          event.target.value
-        ].flat()
-      })
+          event.target.value,
+        ].flat(),
+      });
     } else {
       this.setState({
         disciplineFilters: this.state.disciplineFilters.filter(
-          discipline => discipline !== event.target.value
-        )
-      })
+          (discipline) => discipline !== event.target.value
+        ),
+      });
     }
-  }
+  };
 
-  bookTechnician = technicianId => {
-    const eventId = this.props.match.params.id
+  bookTechnician = (technicianId) => {
+    const eventId = this.props.match.params.id;
     API.addTechToEvent(eventId, technicianId).then(
       this.props.history.push(`/events/${eventId}`)
-    )
-  }
+    );
+  };
 
-  handleFilter = event => {
-    console.log(event.target.name)
-  }
+  handleFilter = (event) => {
+    console.log(event.target.name);
+  };
 
   render() {
     return (
       <>
         <Typography
           style={{ padding: 20 }}
-          variant='h4'
-          color='inherit'
-          align='center'
+          variant="h4"
+          color="inherit"
+          align="center"
         >
           {this.props.match.params.id
             ? "Available Technicians"
@@ -82,7 +82,7 @@ class AllTechniciansContainer extends Component {
 
         <TextField
           style={{ padding: 24 }}
-          placeholder='search by name'
+          placeholder="search by name"
           value={this.state.searchField}
           onChange={this.handleChange}
         >
@@ -91,9 +91,9 @@ class AllTechniciansContainer extends Component {
 
         <Grid container spacing={24} style={{ padding: 24 }}>
           {this.state.loading ? (
-            <CircularProgress style={{ margin: "auto" }} align='center' />
+            <CircularProgress style={{ margin: "auto" }} align="center" />
           ) : (
-            this.searchByName().map(currentTech => (
+            this.searchByName().map((currentTech) => (
               <Grid key={currentTech.id} item s={6} m={4} lg={3}>
                 <TechnicianCard
                   bookTechnician={this.bookTechnician}
@@ -106,8 +106,8 @@ class AllTechniciansContainer extends Component {
           )}
         </Grid>
       </>
-    )
+    );
   }
 }
 
-export default AllTechniciansContainer
+export default AllTechniciansContainer;
