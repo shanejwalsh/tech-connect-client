6b7499c7551f2ea37350addd85a2dfb4e57322ef
Shane Walsh <shanewalsh@hey.com>
2021-09-29 16:12:38 +0100

    lint Event Card

--- src/Components/EventCard.js
+++ src/Components/EventCard.js
@@ -1,90 +1,90 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { withStyles } from "@material-ui/core/styles"
-import Card from "@material-ui/core/Card"
-import CardActions from "@material-ui/core/CardActions"
-import CardContent from "@material-ui/core/CardContent"
-import Button from "@material-ui/core/Button"
-import Typography from "@material-ui/core/Typography"
-import { formatDate } from "../lib/helper"
-import { Link } from "react-router-dom"
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { formatDate } from "../lib/helper";
+import { Link } from "react-router-dom";
 
 const styles = {
-    card: {
-        minWidth: 500
-    },
-    bullet: {
-        display: "inline-block",
-        margin: "0 2px",
-        transform: "scale(0.8)"
-    },
-    title: {
-        fontSize: 14
-    },
-    pos: {
-        marginBottom: 12
-    }
-}
+  card: {
+    minWidth: 500,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
 
 function EventCard(props) {
-    const { classes, event } = props
-    // const bull = <span className={classes.bullet}>•</span>
+  const { classes, event } = props;
+  // const bull = <span className={classes.bullet}>•</span>
 
-    return (
-        <div>
-            <Card
-                raised
-                style={{ minWidth: "280px", maxWidth: "300px", minHeight: 200 }}
-                className={classes.card}
+  return (
+    <div>
+      <Card
+        raised
+        style={{ minWidth: "280px", maxWidth: "300px", minHeight: 200 }}
+        className={classes.card}
+      >
+        <CardContent>
+          <Typography
+            noWrap
+            className={classes.title}
+            color="textSecondary"
+            gutterBottom
+          >
+            {props.isTech ? event.title : event.client}
+          </Typography>
+          <Typography noWrap variant="h5" component="h2">
+            {event.venue}
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            {formatDate(event.start)} - {formatDate(event.end)}
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            Technicians {event.technicians.length}/{event.techs_required}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <span style={{ margin: "auto" }}>
+            <Button
+              variant="contained"
+              color="secondary"
+              component={Link}
+              to={`/events/${event.id}`}
+              size="small"
             >
-                <CardContent>
-                    <Typography
-                        noWrap
-                        className={classes.title}
-                        color='textSecondary'
-                        gutterBottom
-                    >
-                        {props.isTech ? event.title : event.client}
-                    </Typography>
-                    <Typography noWrap variant='h5' component='h2'>
-                        {event.venue}
-                    </Typography>
-                    <Typography className={classes.pos} color='textSecondary'>
-                        {formatDate(event.start)} - {formatDate(event.end)}
-                    </Typography>
-                    <Typography className={classes.pos} color='textSecondary'>
-                        Technicians {event.technicians.length}/{event.techs_required}
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <span style={{ margin: "auto" }}>
-                        <Button
-                            variant='contained'
-                            color='secondary'
-                            component={Link}
-                            to={`/events/${event.id}`}
-                            size='small'
-                        >
-                            More Details
-                        </Button>
-                        <Button
-                            style={{ marginLeft: "20px" }}
-                            variant='contained'
-                            color='secondary'
-                            onClick={() => props.showDeleteDialog(event.id)}
-                            size='small'
-                        >
-                            {props.isTech ? "Cancel Event" : "Delete Event"}
-                        </Button>
-                    </span>
-                </CardActions>
-            </Card>
-        </div>
-    )
+              More Details
+            </Button>
+            <Button
+              style={{ marginLeft: "20px" }}
+              variant="contained"
+              color="secondary"
+              onClick={() => props.showDeleteDialog(event.id)}
+              size="small"
+            >
+              {props.isTech ? "Cancel Event" : "Delete Event"}
+            </Button>
+          </span>
+        </CardActions>
+      </Card>
+    </div>
+  );
 }
 
 EventCard.propTypes = {
-    classes: PropTypes.object.isRequired
-}
+  classes: PropTypes.object.isRequired,
+};
 
-export default withStyles(styles)(EventCard)
+export default withStyles(styles)(EventCard);
